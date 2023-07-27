fa87fdd24b426a32dbd41af29d7b1ec66b9fdce9
dwipu91 <aktruzzamandwiu@gmail.com>
2023-07-27 23:46:04 +0600

    2nd time

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 
 const form = document.getElementById("userForm");
 const msg = document.querySelector(".box .msg")
+const loader = document.getElementById("loader")
+console.log(loader);
 
 form.onsubmit = (e) =>{
     e.preventDefault();
@@ -8,16 +10,22 @@ form.onsubmit = (e) =>{
     const firm_Data = new FormData(e.target);
     const data = Object.fromEntries(firm_Data)
     
+    loader.style.display = "block";
+    
+    setTimeout(function(){
+        loader.style.display = "none";
+        if(!data.Subject || !data.Marks ){
+            msg.innerHTML = createAlert("All fields are not required" )
+        }else{
+            const {gpa, gread} = getResult(data.Marks);
+            
+            msg.innerHTML =     createAlert(`
+            Subject : ${data.Subject} / Marks : ${data.Marks} / GPA : ${gpa} / Gread : ${gread}
+        `, "info")
+        }
+    
+        e.target.reset()
+    }, 3000)
 
-    if(!data.Subject || !data.Marks ){
-        msg.innerHTML = createAlert("All fields are not required" )
-    }else{
-        const {gpa, gread} = getResult(data.Marks);
-        
-        msg.innerHTML =     createAlert(`
-        Subject : ${data.Subject} / Marks : ${data.Marks} / GPA : ${gpa} / Gread : ${gread}
-    `, "info")
-    }
-
-    e.target.reset()
+    
 }
